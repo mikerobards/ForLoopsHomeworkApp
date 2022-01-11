b113b4eafcd477b5f0e69f751b426b04487449d8
Michael Robards <mikerobards@icloud.com>
2022-01-11 14:16:06 -0500

    all requirements met

--- ForLoopsHomework/Program.cs
+++ ForLoopsHomework/Program.cs
@@ -1,4 +1,12 @@
 ﻿
 
+Console.Write("Please enter a comma-separated list of first names (no spaces!): ");
+string data = Console.ReadLine();
 
+List<string> firstNames = data.Split(',').ToList();
+
+for (int i = 0; i < firstNames.Count; i++)
+{
+    Console.WriteLine($"Hello {firstNames[i]}!");
+}
 
